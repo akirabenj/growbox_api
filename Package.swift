1e914189e1f95a901e00e054d2fe1f7dedbcfcd9
rnjin <akirabenj@hotmail.com>
2022-04-08 23:40:50 +0700

    swift-version downgrade

--- Package.swift
+++ Package.swift
@@ -1,32 +1,21 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "GrowboxAPI",
     platforms: [
-       .macOS(.v12)
+       .macOS(.v10_15)
+    ],
+    products: [
+        .executable(name: "Run", targets: ["Run"]),
+        .library(name: "App", targets: ["App"]),
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
-        .target(
-            name: "App",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor")
-            ],
-            swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-            ]
-        ),
-        .executableTarget(name: "Run", dependencies: [.target(name: "App")]),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
-        ])
+        .target(name: "App", dependencies: [.product(name: "Vapor", package: "vapor")]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
